14f7692f75f58c5ed29ee88c1cfa0b9c8e836379
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2025-05-05 23:33:30 -0300

    Add files via upload

--- mapa_geobr_ggplot2.R
+++ mapa_geobr_ggplot2.R
@@ -21,9 +21,29 @@
 
 ## Fazer o cartograma por meio do pacote ggplot2
 
-# Baixar pacotes ---------------------------------------------------------------------------------------------------------------------------
+# Carregar pacotes ---------------------------------------------------------------------------------------------------------------------------
 
 library(geobr) # para obter o shapefile do mapa do Brasil.
 library(ggplot2) # para construir os gráficos.
 library(dplyr) # para a manipulação dos dados.
 library(readxl) # para extrair os dados do excel.
+
+# Baixar shapefile do Brasil ---------------------------------------------------------------------------------------------------------------
+
+brasil <- read_country(year = 2020)
+glimpse(brasil)
+View(brasil)
+
+## Verificar outras funções do pacote geobr
+
+biomas <- geobr::read_biomes()
+glimpse(biomas)
+View(biomas)
+
+## A função geom_sf do ggplot2 usa as coordenadas que estão em 
+## dados_brasil$geom no formato “sfc_MULTIPOLYGON” para fazer as
+## fronteiras dos estados.
+
+ggplot() +
+  geom_sf(data = brasil) +
+  theme_minimal()
