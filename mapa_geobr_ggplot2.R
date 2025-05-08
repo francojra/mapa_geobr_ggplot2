3ebca931a0f4b999aa34b1b33cb6fe69f3b9b5fb
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2025-05-07 23:36:17 -0300

    Add files via upload

--- mapa_geobr_ggplot2.R
+++ mapa_geobr_ggplot2.R
@@ -103,11 +103,12 @@ view(dados_risco_estados)
 
 ggplot(dados_risco_estados, aes(fill = soma_poligono)) +
   geom_sf(color = "black", size = 4) +
-  scale_fill_viridis(option = "magma", direction = -1,
+  scale_fill_viridis(option = "magma", 
+                     direction = -1,
                      name = "Número de Polígonos\nde Áreas de Risco") +
   # scale_fill_gradient(low = "blue", 
   #                     high = "red", 
-  #   name = "Número total \nde Polígonos\nde Áreas de\n Risco") +
+  #                     name = "Número total \nde Polígonos\nde Áreas de\n Risco") +
   xlab("") +  ylab("") +
   labs(title = "Áreas de Risco no Brasil",
        caption = "Fonte: Instituto Brasileiro de Geografia e Estatística (IBGE).") +
